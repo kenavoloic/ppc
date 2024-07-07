cbf3692d855aa1c68ca0da9b43b0c6f7018d7900
kenavoloic <kenavoloic@gmail.com>
2024-07-08 00:23:08 +0200

    shadcn suite + ajout des deux journaux
    shadcn : ajout de fontFamily dans tailwind.config.ts
    
    Documents aux formats org-mode
    ppc_journal.org
    ppc_dbase.org

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,10 @@ const config = {
         "2xl": "1400px",
       },
     },
-    extend: {
+      extend: {
+	  fontFamily: {
+	      sans: ["var(--font-sans)", ...fontFamily.sans],
+	  },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
