ea00dadb47b8220d490a9277b8091b74732f9fa4
kenavoloic <kenavoloic@gmail.com>
2024-07-10 14:25:56 +0200

    Ajout éléments pour l'interface
    shadcn
    
    button, form, label, table, toggle-group, toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,31 @@
 "use client";
 
 import Image from 'next/image'
-import Boutons from '@ui/Boutons';
+//import Boutons from '@ui/Boutons';
+import {Button} from "@/components/ui/button"
+import {ChevronRight, ThumbsUp, ThumbsDown} from "lucide-react"
+
 
 export default function Home() {
     return (
-	<>
-	    <h1>Zlatan was here</h1>
-	    <Boutons />
-	    </>
+	<main className="flex bg-indigo-500 min-h-screen flex-col items-center justify-between p-24">
+	    <Button variant="outline">Blip</Button>
+	    <Button variant="secondary">Blip</Button>
+	    <Button variant="destructive">Blip</Button>
+	    
+	    <Button variant="outline" size="icon">
+	    <ChevronRight className="h-4 w-4" />
+	    </Button>
+
+	    <Button variant="outline" size="icon">
+	    <ThumbsUp className="h-4 w-4" />
+	    </Button>
+
+	    <Button variant="destructive" size="icon">
+	    <ThumbsDown className="h-4 w-4"  />
+	    </Button>
+
+	    </main>
 	    
     );
 }
