0da7f3360ffdf458b1f8dc8f347c156bcb2842fc
kenavoloic <kenavoloic@gmail.com>
2024-07-11 19:57:32 +0200

    Suppression de Chifoumi.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import Image from 'next/image'
-import Boutons from '@ui/Boutons';
 import {Button} from '@ui/button';
 import {Input} from '@ui/input';
 import {Label} from '@ui/label';
 import {Toggle} from '@ui/toggle';
 import {ToggleGroup, ToggleGroupItem} from '@ui/toggle-group';
-//import {Button} from "@/components/ui/button"
-import {ChevronRight, ThumbsUp, ThumbsDown} from "lucide-react"
+import {ThumbsUp, ThumbsDown} from "lucide-react"
 import Chifoumi from '@components/chifoumi';
 
 
@@ -16,8 +14,7 @@ export default function Home() {
   return (
     <main className="flex bg-indigo-500 min-h-screen flex-col items-center justify-between p-24">
       <Chifoumi />
-
-	    </main>
+    </main>
 	    
   );
 }
