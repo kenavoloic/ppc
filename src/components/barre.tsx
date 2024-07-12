93aa2de027ca81e3bf1afe5def68f4776def897f
kenavoloic <kenavoloic@gmail.com>
2024-07-12 19:04:46 +0200

    Fonctionnalités responsives du menu navigation : icônes et texte

--- src/components/barre.tsx
+++ src/components/barre.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import {Button} from "@components/ui/button";
 import { House, LogIn, LogOut, ThumbsUp, Trophy } from "lucide-react"
-//import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger} from "@components/ui/dropdown-menu";
 import {NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLabel, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle} from "@components/ui/navigation-menu";
 
 import Link from "next/link";
@@ -10,45 +9,45 @@ import {ModeToggle} from "@components/modeToggle";
 
 const  Barre = () => {
   return (
-    <div className="bg-primary dark:bg-slate-700 text-white py-2 px-2 flex justify-between rounded-lg ">
-      <div className="px-1 py-1">
+    <div className="bg-primary dark:bg-slate-700 text-white p-2 flex justify-between rounded-lg ">
+      <div className="w-full">
 
 	<NavigationMenu className="flex justify-between">
 	  <NavigationMenuList>
 	  <NavigationMenuItem>
   <Link href="/" legacyBehavior passHref>
     <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-      <span className="hidden text-black dark:text-white">Accueil</span>
-      <House className="md:flex text-black dark:text-white"/>
+      <House className="flex text-black dark:text-white md:hidden lg:flex lg:mr-2"/>
+      <span className="hidden md:flex md:text-black md:dark:text-white">Accueil</span>
     </NavigationMenuLink>
   </Link>
 </NavigationMenuItem>
 
 	  <NavigationMenuItem>
-  <Link href="/">
+  <Link href="/" legacyBehavior passHref>
     <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-      <span className="hidden text-black dark:text-white">Register</span>
-      <ThumbsUp className="md:flex text-black dark:text-white" />
+      <ThumbsUp className="flex text-black dark:text-white md:hidden lg:flex lg:mr-2" />
+      <span className="hidden md:flex md:text-black md:dark:text-white">Register</span>
     </NavigationMenuLink>
   </Link>
 </NavigationMenuItem>
 
   
 	  <NavigationMenuItem>
-  <Link href="/">
+  <Link href="/" legacyBehavior passHref>
     <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-      <span className="hidden text-black dark:text-white">Login</span>
-      <LogIn className="md:flex text-black dark:text-white" />
+      <LogIn className="flex text-black dark:text-white md:hidden lg:flex lg:mr-2" />
+      <span className="hidden md:flex md:text-black md:dark:text-white">Login</span>
     </NavigationMenuLink>
   </Link>
 </NavigationMenuItem>
 
 
 	  <NavigationMenuItem>
-  <Link href="/">
+  <Link href="/" legacyBehavior passHref>
     <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-      <span className="hidden text-black dark:text-white">Logout</span>
-      <LogOut className="md:flex text-black dark:text-white" />
+      <LogOut className="flex text-black dark:text-white md:hidden lg:flex lg:mr-2" />
+      <span className="hidden md:flex md:text-black md:dark:text-white">Logout</span>
     </NavigationMenuLink>
   </Link>
 </NavigationMenuItem>
